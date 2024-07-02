39b1d0bc68fa02ee714a28800542c67241df6b65
LSG <tkdruddl20@naver.com>
2024-07-02 11:45:06 +0900

    처음으로 복구

--- src/main/java/com/example/library/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.library.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.library.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-  public User findByEmailAndPwd(String email, String pwd);
-}
