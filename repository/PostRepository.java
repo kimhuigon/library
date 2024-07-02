ae3b1726da4ff85d56d06bce1615d1d2d9ba667c
Falin4789 <koo7264@gmail.com>
2024-07-02 16:40:39 +0900

    Delete repository directory

--- repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.library.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.library.entity.Post;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
